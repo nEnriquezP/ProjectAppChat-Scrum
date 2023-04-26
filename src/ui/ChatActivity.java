05bbfcf9e90758dd0a954d71a0cc4031bc7b393a
nelsonenriquez <nenriquezp@miumg.edu.gt>
2023-04-26 00:17:40 -0600

    Primeras lineas de desarrollo #12

---
+++ src/ui/ChatActivity.java
@@ -0,0 +1,49 @@
+package com.chatroom.ui;
+
+import java.util.List;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.border.AbstractBorder;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+
+import com.chatroom.client.ClientModel;
+import com.chatroom.configuration.Config;
+import com.chatroom.models.Request;
+import com.chatroom.models.Response;
+import com.chatroom.others.LogFileWriter;
+import com.chatroom.others.Message;
+import com.chatroom.others.TextBubbleBorder;
+import com.sun.org.apache.xml.internal.serializer.utils.MsgKey;
+import java.awt.Font;
+import java.awt.Image;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+// emoji ☻ ☹
+
+
+
+public class ChatActivity {
+}
